79ec983d7697a20ef79252c09e2b74458388265c
Mateusdev7 <mateusdev1337@gmail.com>
2022-05-31 16:56:36 -0300

    Parte Inicial do desafio

---
+++ udemy_secao13_enumeracoes_e_composicoes/desafio_enum_pedido/desafio/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+
+
+        entrada.close();
+    }
+}
---
+++ udemy_secao13_enumeracoes_e_composicoes/desafio_enum_pedido/desafio/src/entities/Order.java
@@ -0,0 +1,10 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Order {
+    private OrderStatus status;
+    private Date moment;
+}
