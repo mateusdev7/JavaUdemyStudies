77f7743a261184bbcf381c0203d0368e4b8e2dea
Mateusdev7 <mateusdev1337@gmail.com>
2022-05-31 19:13:04 -0300

    Classes do desafio criada

--- udemy_secao13_enumeracoes_e_composicoes/desafio_enum_pedido/desafio/src/entities/Order.java
+++ udemy_secao13_enumeracoes_e_composicoes/desafio_enum_pedido/desafio/src/entities/Order.java
@@ -2,9 +2,67 @@ package entities;
 
 import entities.enums.OrderStatus;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Order {
     private OrderStatus status;
     private Date moment;
+
+    private Client client;
+    private List<OrderItem> items = new ArrayList<>();
+
+    public Order(){
+    }
+
+    public Order(OrderStatus status, Date moment, Client client) {
+        this.status = status;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+    }
+
+    public double total(){
+        double sum = 0.0;
+        for(OrderItem item : items) {
+            sum += item.subTotal();
+        }
+        return sum;
+    }
 }
--- udemy_secao13_enumeracoes_e_composicoes/desafio_enum_pedido/desafio/src/entities/OrderItem.java
+++ udemy_secao13_enumeracoes_e_composicoes/desafio_enum_pedido/desafio/src/entities/OrderItem.java
@@ -5,12 +5,15 @@ public class OrderItem {
     private Integer quantity;
     private Double price;
 
+    private Product product;
+
     public OrderItem(){
     }
 
-    public OrderItem(Integer quantity, Double price) {
+    public OrderItem(Integer quantity, Double price, Product product) {
         this.quantity = quantity;
         this.price = price;
+        this.product = product;
     }
 
     public Integer getQuantity() {
@@ -29,7 +32,27 @@ public class OrderItem {
         this.price = price;
     }
 
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
     public double subTotal() {
         return quantity * price;
     }
+
+    @Override
+    public String toString() {
+        return product.getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity: "
+                + quantity +
+                ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
+
 }
